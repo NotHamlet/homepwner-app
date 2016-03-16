22a903e710c47303e9bb68a6a8036ae282fda0f9
stephen <stephen.a.atwood@gmail.com>
2016-03-16 00:48:59 -0500

    Completed Chapter 9, setting up basic TableView functionality

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,32 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Stephen Atwood on 3/16/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    
+    
+}
